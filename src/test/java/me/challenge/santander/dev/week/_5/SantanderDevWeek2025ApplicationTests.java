bfff7f424b8b442eddb7307aae53043cdc341037
daniel almeida <134938468+dan1el-alme1da@users.noreply.github.com>
2025-05-26 14:13:39 -0300

    Initial commit

---
+++ src/test/java/me/challenge/santander/dev/week/_5/SantanderDevWeek2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.challenge.santander.dev.week._5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWeek2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
